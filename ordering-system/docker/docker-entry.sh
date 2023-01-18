e0d044a8f59d1d5739de0e3091dae3f145dc676f
amoore3234 <akmoore@cpp.edu>
2023-01-17 16:43:10 -0800

    Database migration
     - Implement liquidbase to create tables.
     - Added migration command to the docker-entry shell.

--- ordering-system/docker/docker-entry.sh
+++ ordering-system/docker/docker-entry.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+set -e
+if ! [ -z ${AUTO_MIGRATE+x} ]; then
+  /usr/bin/java ${JAVA_OPTS} -jar circuit-mgmt.jar db migrate config.yml
+fi
 exec /usr/bin/java ${JAVA_OPTS} -jar ordering-system.jar ${@: -server} config.yml
