6ba2a1c70fb509c7666c97e486b9d37d42e9767a
amoore3234 <akmoore@cpp.edu>
2023-02-03 20:01:00 -0800

    Docker image and database configurations.
    - Add data source factory in config class.
    - Modified Dockerfile and docker-entry to satisfy image build.
    - Modified config file to include database properties.

--- ordering-system/docker/docker-entry.sh
+++ ordering-system/docker/docker-entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 if ! [ -z ${AUTO_MIGRATE+x} ]; then
-  /usr/bin/java ${JAVA_OPTS} -jar circuit-mgmt.jar db migrate config.yml
+  /usr/bin/java ${JAVA_OPTS} -jar ordering-system-1.0-SNAPSHOT.jar db migrate config.yml
 fi
-exec /usr/bin/java ${JAVA_OPTS} -jar ordering-system.jar ${@: -server} config.yml
+exec /usr/bin/java ${JAVA_OPTS} -jar ordering-system-1.0-SNAPSHOT.jar ${@:-server} config.yml
