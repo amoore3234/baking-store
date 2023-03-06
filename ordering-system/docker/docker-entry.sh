f4eedcf76395f91e6f14a2138f50019d26364748
Anthony Moore <akmoore@cpp.edu>
2023-03-06 11:54:40 -0800

    Configure docker compose file to include liquibase
    - Removed db migrate command.
    - Added liquibase folder that includes a changelog file.
    - Modified env file to only have the POSTGRES_USER and PASSWORD variables

--- ordering-system/docker/docker-entry.sh
+++ ordering-system/docker/docker-entry.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-if ! [ -z ${AUTO_MIGRATE+x} ]; then
-  /usr/bin/java ${JAVA_OPTS} -jar ordering-system-1.0-SNAPSHOT.jar db migrate config.yml
-fi
+
 exec /usr/bin/java ${JAVA_OPTS} -jar ordering-system-1.0-SNAPSHOT.jar ${@:-server} config.yml
