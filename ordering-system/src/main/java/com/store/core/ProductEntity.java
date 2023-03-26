307698ea071b6d89460fa9af754993bad7a775b5
Anthony Moore <akmoore@cpp.edu>
2023-03-26 11:06:43 -0700

    Modified product entity class.
    - Added Json property annotations.
    - Implement getter and setters for carts and orders variables.

--- ordering-system/src/main/java/com/store/core/ProductEntity.java
+++ ordering-system/src/main/java/com/store/core/ProductEntity.java
@@ -1,5 +1,6 @@
 package com.store.core;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -18,33 +19,45 @@ public class ProductEntity implements Serializable {
 
   @Id
   @Nullable
+  @JsonProperty
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "product_id", nullable = false, unique = true)
   private long id;
 
   @Nullable
+  @JsonProperty
   @Column(name = "product_name")
   private String productName;
 
   @Nullable
+  @JsonProperty
   @Column(name = "product_type")
   private String productType;
 
   @Nullable
+  @JsonProperty
   @Column(name = "product_price")
   private double productPrice;
 
   @Nullable
+  @JsonProperty
   @Column(name = "product_description")
   private String productDescription;
 
   @Nullable
+  @JsonProperty
   @Column(name = "product_quantity")
   private int productQuantity;
 
   @Nullable
+  @JsonProperty
   @OneToMany(mappedBy = "product")
   private List<OrderEntity> orders;
+
+  @Nullable
+  @JsonProperty
+  @OneToMany(mappedBy = "product")
+  private List<CartEntity> carts;
 
   public ProductEntity() {
   }
@@ -103,10 +116,27 @@ public class ProductEntity implements Serializable {
     this.productQuantity = productQuantity;
   }
 
+  @Nullable
+  public List<OrderEntity> getOrders() {
+    return orders;
+  }
+
+  public void setOrders(@Nullable List<OrderEntity> orders) {
+    this.orders = orders;
+  }
+
+  @Nullable
+  public List<CartEntity> getCarts() {
+    return carts;
+  }
+
+  public void setCarts(@Nullable List<CartEntity> carts) {
+    this.carts = carts;
+  }
+
   @Override
   public int hashCode() {
-    return Objects.hash(id, productName, productType, productPrice,
-      productDescription, productQuantity, orders);
+    return Objects.hash(id, productName, productType, productPrice, productDescription, productQuantity, orders, carts);
   }
 
   @Override
@@ -121,16 +151,15 @@ public class ProductEntity implements Serializable {
     return id == other.id && Objects.equals(productName, other.productName)
         && Objects.equals(productType, other.productType)
         && Double.doubleToLongBits(productPrice) == Double.doubleToLongBits(other.productPrice)
-        && Objects.equals(productDescription, other.productDescription)
-        && productQuantity == other.productQuantity
-        && Objects.equals(orders, other.orders);
+        && Objects.equals(productDescription, other.productDescription) && productQuantity == other.productQuantity
+        && Objects.equals(orders, other.orders) && Objects.equals(carts, other.carts);
   }
 
   @Override
   public String toString() {
-    return "ProductEntity [id=" + id + ", productName=" + productName + ", productType="
-      + productType + ", productPrice=" + productPrice + ", productDescription="
-      + productDescription + ", productQuantity=" + productQuantity + ", orders=" + orders + "]";
+    return "ProductEntity [id=" + id + ", productName=" + productName + ", productType=" + productType
+        + ", productPrice=" + productPrice + ", productDescription=" + productDescription + ", productQuantity="
+        + productQuantity + ", orders=" + orders + ", carts=" + carts + "]";
   }
 
 }
